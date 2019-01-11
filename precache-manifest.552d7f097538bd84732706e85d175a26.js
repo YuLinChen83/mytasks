241098458b2d13337ca6e898ee18372b465259bc
Yu Lin Chen <tiffany830326@gmail.com>
2019-01-12 03:03:16 +0800

    Updates

---
+++ precache-manifest.552d7f097538bd84732706e85d175a26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fc04e86528ee61452fe",
+    "url": "/mytasks/static/js/runtime~main.9fc04e86.js"
+  },
+  {
+    "revision": "53ba0deaf6440f3299fd",
+    "url": "/mytasks/static/js/main.53ba0dea.chunk.js"
+  },
+  {
+    "revision": "5e76e863b1002118f4ae",
+    "url": "/mytasks/static/js/1.5e76e863.chunk.js"
+  },
+  {
+    "revision": "53ba0deaf6440f3299fd",
+    "url": "/mytasks/static/css/main.fbd812cf.chunk.css"
+  },
+  {
+    "revision": "8d7ab9113170b31e1bb1bd9d5b4f7338",
+    "url": "/mytasks/index.html"
+  }
+];
